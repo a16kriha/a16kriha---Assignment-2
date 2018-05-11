9416c18697cb530ed4d36343d5f5699d33c46e4d
Kristoffer <a16kriha@student.his.se>
2018-05-11 15:23:42 +0200

    Sends corresponding data in toast

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -2,8 +2,12 @@ package org.brohede.marcus.listviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,14 +25,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] myArray = mountainNames;
-        List<String> listData = new ArrayList<String>(Arrays.asList(myArray));
+        List<String> listData = new ArrayList<String>(Arrays.asList(mountainNames));
         ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),R.layout.list_item_textview,R.id.my_item_textview, listData);
 
         ListView myListView = (ListView)findViewById(R.id.my_listview);
         myListView.setAdapter(adapter);
 
-        adapter.add("Greger");
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String temp = "Location: " + mountainLocations[position] + ", Height: " + String.valueOf(mountainHeights[position]);
+
+                Toast.makeText(getApplicationContext(), temp, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
         // 6. Style the ListView items according to Material Design
         //    See: https://material.io/guidelines/components/lists.html#lists-specs
         //    Look for "singel line specs" for "text only" lists and modify the list_item_textview
